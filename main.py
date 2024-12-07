918a688a9a2078b3ae5944d77c97e72b521269d7
Laksiya <laksiya.balasubramaniam@adig.no>
2024-12-07 13:26:19 +0100

    simple button api and interface

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from requests import Response
 from flask import Flask, render_template, jsonify
-import cv2  # if using OpenCV for camera capture
-
 app = Flask(__name__)
 
 # Route for the main page
@@ -25,15 +23,5 @@ def button3_action():
     # Add your button 3 logic here
     return jsonify({'message': 'Button 3 action completed'})
 
-@app.route('/api/get-frame')
-def get_frame():
-    camera_url = f'rtsp://{username}:{password}@{camera_ip}:554/profile2/media.smp'
-    # Open the camera stream
-    cap = cv2.VideoCapture(camera_url)
-    # Capture frame from your camera/source
-    ret, frame = cap.read()
-    # Convert to jpg
-    ret, jpeg = cv2.imencode('.jpg', frame)
-    return Response(jpeg.tobytes(), mimetype='image/jpeg')
 if __name__ == '__main__':
     app.run(debug=True)
